2f9336259bc54ff967680e9c5850e258d77cccc5
Nate <eclypso@aol.com>
2019-04-05 14:21:27 -0400

    styling changes

--- react-final-project/src/Components/Header.js
+++ react-final-project/src/Components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     
     <div className="header">
         <i className="material-icons mobile__menu__button">menu</i>
-        <img className ="avatar__image" src="https://farm2.staticflickr.com/1723/42703297172_fd3e3933d6.jpg" />
+        <img className ="avatar__image" src="https://4.bp.blogspot.com/-ys1MsCf3fak/WkkufJEuZOI/AAAAAAAAAnY/iHnu2QAQOmoniFuuTLWUaoz1Eq0Z0v6WgCEwYBhgL/s1600/bitmoji-20171231083554.png" alt="Your avatar" />
     </div>
     );
 }
--- react-final-project/src/Components/PostCleanUp.js
+++ react-final-project/src/Components/PostCleanUp.js
@@ -4,7 +4,10 @@ export default (props) =>
     <div className="postCleanUp-feed">
         <div class="postCleanUp-feed-card">
             <div className="postCleanUp">
-                <h3 className="postCleanUp__location">{props.postCleanUp.location} {props.postCleanUp.id}</h3>
+                <div className="postCleanUp__header">
+                    <img className ="postCleanUp__avatar" src="https://farm2.staticflickr.com/1723/42703297172_fd3e3933d6.jpg" />  
+                    <h3 className="postCleanUp__location">{props.postCleanUp.location} {props.postCleanUp.id}</h3>
+                </div>
                 <img className="postCleanUp__image" src={props.postCleanUp.image} alt="CleanUp Photo" />
                 <button class="postCleanUp-feed__button" type="button">
                     <i class="material-icons-outlined">
