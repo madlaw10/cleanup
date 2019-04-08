137dce08e898a7dcb04d2257d9b51fcbd604eb8d
Bini <abigiyabiniyam@gmail.com>
2019-04-08 10:47:42 -0400

    pre clean up button

--- react-final-project/src/Components/precleanups/PreCleanUp.js
+++ react-final-project/src/Components/precleanups/PreCleanUp.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './precleanups.css'
 
-export default (props) => 
+export default (props) => {
+const preCleanUpId = props.preCleanUp.id
+return(
 <div className = "preCleanUp-feed">
     <div className= "preCleanUp-feed-card">
         <div className="preCleanUp">
             <h3 className="preCleanUp__location"> {props.preCleanUp.location} {props.preCleanUp.id}</h3>
             <p className="preCleanUp__description">{props.preCleanUp.description}</p>
+            {props.currentLocation === "precleanups" && <button className="preCleanUp-toggle__button" onClick={() => props.getPreCleanUp(preCleanUpId)}>View More</button>}
         </div>
     </div>
 </div>
+)
+}
--- react-final-project/src/Components/precleanups/PreCleanUps.js
+++ react-final-project/src/Components/precleanups/PreCleanUps.js
@@ -4,5 +4,5 @@ import './precleanups.css'
 
 export default (props) => {
     return props.preCleanUps
-        .map(preCleanUp => <PreCleanUp key={preCleanUp.id} preCleanUp={preCleanUp} />)
+        .map(preCleanUp => <PreCleanUp key={preCleanUp.id} preCleanUp={preCleanUp} getPreCleanUp={props.getPreCleanUp} currentLocation={props.currentLocation}/>)
 }
