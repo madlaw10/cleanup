fad2e17665a2304a91bd0587a0f3c3de45f2834a
Bini <abigiyabiniyam@gmail.com>
2019-04-10 11:47:18 -0400

    added and sorted the precleanups

--- api-final-project/src/main/java/org/wecancodeit/finalproject/repositories/PreCleanUpRepository.java
+++ api-final-project/src/main/java/org/wecancodeit/finalproject/repositories/PreCleanUpRepository.java
@@ -1,5 +1,7 @@
 package org.wecancodeit.finalproject.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.wecancodeit.finalproject.models.PreCleanUp;
@@ -9,5 +11,6 @@ public interface PreCleanUpRepository extends CrudRepository<PreCleanUp, Long> {
 
 	PreCleanUp findByLocation(String location);
 	
+	List<PreCleanUp> OrderByScheduledDateAsc();  
 
 }
