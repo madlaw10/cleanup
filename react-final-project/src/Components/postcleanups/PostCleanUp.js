28deb6f1324460e3dd57fec89872a76ca63a3563
Madison Lawton <madison.e.lawton@gmail.com>
2019-04-11 11:41:16 -0400

    votinggit statusgit status

--- react-final-project/src/Components/postcleanups/PostCleanUp.js
+++ react-final-project/src/Components/postcleanups/PostCleanUp.js
@@ -13,19 +13,23 @@ export default (props) => {
                         <h3 className="postCleanUp__location">{props.postCleanUp.location} {props.postCleanUp.id}</h3>
                     </div>
                     <img className="postCleanUp__image" src={props.postCleanUp.image} alt="CleanUp" />
+                    
+                    {props.currentLocation === "postcleanups" &&
                     <div className="postCleanUp__vote-container">
-                        <button className="postCleanUp-feed__button" type="button">
+                        <button className="postCleanUp-feed__button" type="button" onClick={() => props.voteDown(postCleanUpId)}>
                             <i className="material-icons-outlined postCleanUp-feed__button-icon-down">
                                 thumb_down
                         </i>
                         </button>
                         <p className="postCleanUp__votes">{props.postCleanUp.count}</p>
-                        <button className="postCleanUp-feed__button" type="button">
+                        <button className="postCleanUp-feed__button" type="button" onClick={() => props.voteUp(postCleanUpId)}>
                             <i className="material-icons-outlined postCleanUp-feed__button-icon-up">
                                 thumb_up
                         </i>
                         </button>
                     </div>
+                    }
+
                     <p className="postCleanUp__caption">{props.postCleanUp.caption}</p>
                     {props.currentLocation === "postcleanups" && <button className="postCleanUp-toggle__button" onClick={() => props.getPostCleanUp(postCleanUpId)}>View More</button>}
                     <div>
