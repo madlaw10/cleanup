e81e30aca52eac17a12a837558851e1f5aa3aafe
Nate <eclypso@aol.com>
2019-04-12 13:31:01 -0400

    Added to Initializer and added images to project

--- api-final-project/src/main/java/org/wecancodeit/finalproject/Initializer.java
+++ api-final-project/src/main/java/org/wecancodeit/finalproject/Initializer.java
@@ -29,21 +29,35 @@ public class Initializer implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {
+		// Users
 		User userZero = userRepo.save(new User("binieth", "https://files.slack.com/files-pri/T14LST83D-FHXBH1XHD/image_from_ios.jpg"));
 		User userOne = userRepo.save(new User("dexter", "https://files.slack.com/files-pri/T14LST83D-FHTNXP05T/image-11.png"));
 		User userTwo = userRepo.save(new User("madlaw", "https://files.slack.com/files-pri/T14LST83D-FHGCEB70A/bitmoji-20190411022708.png"));
 		User userThree = userRepo.save(new User("nate", "https://files.slack.com/files-pri/T14LST83D-FHXM8020N/bitmoji-20190411024503.png"));
 		User userFour = userRepo.save(new User("eythan", "https://files.slack.com/files-pri/T14LST83D-FHMFRA16V/file.jpg"));
-		PreCleanUp cleanUpZero = preCleanUpRepo.save(new PreCleanUp(LocalDate.now(), "locationZero", "descriptionZero"));
-		PostCleanUp cleanUpOne = postCleanUpRepo.save(new PostCleanUp("https://static.boredpanda.com/blog/wp-content/uploads/2019/03/trashtag-challenge-people-clean-surroundings-108-5c8659a6653f5__700.jpg", "locationOne", "River side clean up when well with great friends!"));
-		PostCleanUp cleanUpTwo = postCleanUpRepo.save(new PostCleanUp("https://media.globalcitizen.org/thumbnails/e4/15/e415fee3-e28a-4d6d-9ce1-ba303d3d5dab/screen_shot_2019-03-11_at_53455_pm.png__1500x670_q85_crop_subsampling-2.png", "locationTwo", "captionTwo"));
-		PostCleanUp cleanUpThree = postCleanUpRepo.save(new PostCleanUp("https://static.boredpanda.com/blog/wp-content/uploads/2019/03/trashtag-challenge-people-clean-surroundings-104-5c865855c801d__700.jpg", "locationThree", "captionThree"));
-		PostCleanUp cleanUpFour = postCleanUpRepo.save(new PostCleanUp("https://pbs.twimg.com/profile_images/933918958/cuw_logo_en_200x200_400x400.jpg", "locationFour", "captionFour"));
-		commentRepo.save(new CleanUpComment("contentZero", cleanUpZero));
-		commentRepo.save(new CleanUpComment("contentOne", cleanUpOne));
-		commentRepo.save(new CleanUpComment("contentTwo", cleanUpTwo));
-		commentRepo.save(new CleanUpComment("contentThree", cleanUpThree));
-		commentRepo.save(new CleanUpComment("contentFour", cleanUpFour));
+
+		// Pre CleanUps
+		PreCleanUp preCleanUpZero = preCleanUpRepo.save(new PreCleanUp(LocalDate.now(), "Goodale Park", "We are all gathering at Goodale Park again on 4/28 to pick up any trash that has accumulated since our last CleanUp!  Please join us if you're available!"));
+		PreCleanUp preCleanUpOne = preCleanUpRepo.save(new PreCleanUp(LocalDate.now(), "Park of Roses", "Hey guys!  Park of Roses is beautiful, but we've noticed some trash blowing in from recent storms.  Let's get together on 4/29 and CleanUp!"));
+		PreCleanUp preCleanUpTwo = preCleanUpRepo.save(new PreCleanUp(LocalDate.now(), "Portal Park", "We've got another CleanUp going! Head over on 4/29 to help out if you can."));
+		PreCleanUp preCleanUpThree = preCleanUpRepo.save(new PreCleanUp(LocalDate.now(), "Glen Echo Park", "Saturday, May 4th.  Let's do this!"));
+		PreCleanUp preCleanUpFour = preCleanUpRepo.save(new PreCleanUp(LocalDate.now(), "Highbanks", "Starts at 11am on 5/5.  We'll meet in the first parking lot after the welcome center."));
+
+
+		// Post CleanUps
+		PostCleanUp cleanUpOne = postCleanUpRepo.save(new PostCleanUp("https://static.boredpanda.com/blog/wp-content/uploads/2019/03/trashtag-challenge-people-clean-surroundings-108-5c8659a6653f5__700.jpg", "Riverside Park", "Riverside clean up when well with great friends!"));
+		PostCleanUp cleanUpTwo = postCleanUpRepo.save(new PostCleanUp("https://media.globalcitizen.org/thumbnails/e4/15/e415fee3-e28a-4d6d-9ce1-ba303d3d5dab/screen_shot_2019-03-11_at_53455_pm.png__1500x670_q85_crop_subsampling-2.png", "Miami Beach", "Beaches look better without trash."));
+		PostCleanUp cleanUpThree = postCleanUpRepo.save(new PostCleanUp("https://static.boredpanda.com/blog/wp-content/uploads/2019/03/trashtag-challenge-people-clean-surroundings-104-5c865855c801d__700.jpg", "Lake Erie", "Cleaned up the beach this weekend."));
+		PostCleanUp cleanUpFour = postCleanUpRepo.save(new PostCleanUp("https://static.boredpanda.com/blog/wp-content/uploads/2019/03/xji67arzwbl21-5c86559301cbe__700.jpg", "Manila Bay", " January 27 CleanUp at Manila Bay."));
+		PostCleanUp cleanUpFive = postCleanUpRepo.save(new PostCleanUp("https://static.boredpanda.com/blog/wp-content/uploads/2019/03/trashtag-challenge-people-clean-surroundings-102-5c8657638ca49__700.jpg", "Potomac River", "Cleaned up DC a bit."));
+
+
+		// Comments
+		commentRepo.save(new CleanUpComment("This is awesome!!  #trashtag", cleanUpOne));
+		commentRepo.save(new CleanUpComment("Looks incredible, let's keep CBUS clean!", cleanUpTwo));
+		commentRepo.save(new CleanUpComment("Wow!!", cleanUpThree));
+		commentRepo.save(new CleanUpComment(":heart: this!!!  #trashtag #cleanUp", cleanUpFour));
+		commentRepo.save(new CleanUpComment("Great work guys!", cleanUpFive));
 		
 		
 	}
