01969c9d60839163950227e24a46c9e78b8c1bf4
Nate <eclypso@aol.com>
2019-04-17 14:03:33 -0400

    added custom map markers

--- react-final-project/src/Components/MapContainer.js
+++ react-final-project/src/Components/MapContainer.js
@@ -1,26 +1,79 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-import '../css/map.css';
+import React from 'react';
+import { GoogleApiWrapper, Map, Marker, InfoWindow } from 'google-maps-react';
+import '../css/map.css'
 
-export class MapContainer extends Component {
+const MarkersList = props => {
+  const { locations, ...markerProps } = props;
+  return (
+    <span>
+      {locations.map((location, i) => {
+        return (
+          <Marker
+            key={i}
+            {...markerProps}
+            position={{ lat: location.lat(), lng: location.lng() }}
+          />
+        );
+      })}
+    </span>
+  );
+};
+
+class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locations: []
+    };
+    this.handleMapClick = this.handleMapClick.bind(this);
+  }
+
+  handleMapClick = (ref, map, ev) => {
+    const location = ev.latLng;
+    this.setState(prevState => ({
+      locations: [...prevState.locations, location]
+    }));
+    map.panTo(location);
+  };
+
   render() {
     return (
       <div className="map-view">
-        <section className="map-container">
-        <Map
-          google={this.props.google}
-          zoom={14}
-          initialCenter={{
-          lat: 39.9611,
-          lng: -82.9987
-          }}
-        />
-        </section>
+        <div className="map-container">
+          <Map
+            google={this.props.google}
+            className={"map"}
+            zoom={this.props.zoom}
+            initialCenter={{
+              lat: 39.9611,
+              lng: -82.9987
+              }}
+              styles={ [    
+                {
+                    featureType: "poi",
+                    elementType: "labels",
+                    stylers: [{ visibility: "off" }]
+                }
+            ]}
+            onClick={this.handleMapClick}
+          >
+          <MarkersList locations={this.state.locations} icon="https://raw.githubusercontent.com/madlaw10/final-project/master/react-final-project/src/media/cleanup__map-marker.png" />
+
+          <Marker
+          position = {{ lat: 39.9611, lng: -82.9987 }}
+          icon="https://raw.githubusercontent.com/madlaw10/final-project/master/react-final-project/src/media/cleanup__map-marker.png"
+          />
+
+
+          </Map>
+        </div>
       </div>
     );
   }
 }
-
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'
+  apiKey: 'AIzaSyA8x0vAXqI7h6s44_Ro_g_6FWMCadssyOs'
 })(MapContainer);
+
+
+
